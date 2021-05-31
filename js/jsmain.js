59ecfbb3577cca6c6fdec5695f8b845f5f6d72b2
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-05-31 17:05:37 +0200

    addhtml

--- js/jsmain.js
+++ js/jsmain.js
@@ -1,5 +1,22 @@
-'use strict';
+"use strict";
 
-console.log ('Holaqtal');
+const url = "https://randomuser.me/api/?results=10";
+const userList = document.querySelector(".js-userslist");
+let userResults = [];
 
-// alert ('welcome!');
+function obtainDates() {
+  const userRandom = userList.value;
+  fetch(url + userRandom)
+    .then((response) => response.json())
+    .then((data) => {
+      userResults = data; //guardo datos de mi peticion en array global
+    });
+
+  for (const userList of userResults) {
+    let eachResult = "";
+    eachResult += `<li class = "js-listUsers"  id="${userResults.results.name}">`;
+  }
+  console.log(userResults);
+
+  eachResult += "</li>";
+}
