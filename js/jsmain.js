7b538afa14fec0fd417fb3031c4846297413e6bd
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-06-01 13:46:23 +0200

    painthtml

--- js/jsmain.js
+++ js/jsmain.js
@@ -5,18 +5,22 @@ const userList = document.querySelector(".js-userslist");
 let userResults = [];
 
 function obtainDates() {
-  const userRandom = userList.value;
-  fetch(url + userRandom)
-    .then((response) => response.json())
+  // const userRandom = userList.value;
+  fetch(url)
+    .then(response => response.json())
     .then((data) => {
-      userResults = data; //guardo datos de mi peticion en array global
+      console.log(data);
+      userResults = data; //guardo datos 
     });
+
 
   for (const userList of userResults) {
     let eachResult = "";
     eachResult += `<li class = "js-listUsers"  id="${userResults.results.name}">`;
   }
-  console.log(userResults);
+
 
   eachResult += "</li>";
 }
+
+console.log(userList);
