08ce12ef4b83f3a8e45c716b43e8e8132bad4b5c
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-06-01 16:10:24 +0200

    run changes

--- js/jsmain.js
+++ js/jsmain.js
@@ -3,24 +3,22 @@
 const url = "https://randomuser.me/api/?results=10";
 const userList = document.querySelector(".js-userslist");
 let userResults = [];
+let eachResult = "";
 
 function obtainDates() {
-  // const userRandom = userList.value;
   fetch(url)
-    .then(response => response.json())
+    .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      userResults = data; //guardo datos 
+      userResults = data; //guardo datos
     });
 
-
   for (const userList of userResults) {
-    let eachResult = "";
     eachResult += `<li class = "js-listUsers"  id="${userResults.results.name}">`;
   }
 
-
   eachResult += "</li>";
 }
+obtainDates();
 
 console.log(userList);
