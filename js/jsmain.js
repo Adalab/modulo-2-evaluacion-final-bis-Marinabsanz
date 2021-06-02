c3f7c6321cd507b8e1ba571f2f0be39df72d66f5
Marina Benítez Sánchez <marinabsanz@gmail.com>
2021-06-02 15:10:29 +0200

    save

--- js/jsmain.js
+++ js/jsmain.js
@@ -3,26 +3,20 @@
 const url = "https://randomuser.me/api/?results=10";
 const userList = document.querySelector(".js-userslist");
 let contenido = document.querySelector(".js-contenido");
-let userResults = [];
-//  userList.value = userResults.value;
+
 let html = "";
-
-
 
 function obtainDates() {
   fetch(url)
     .then((response) => response.json())
     .then((data) => showResults(data));
-
-  console.log(showResults);
 }
 
 function showResults(eachResult) {
- 
-  eachResult.forEach (profile => {
+  let html = "";
+
+  eachResult.forEach((profile) => {
     const { user, location } = profile;
- 
-    let html = "";
 
     html += `
     <li>  
@@ -30,11 +24,9 @@ function showResults(eachResult) {
     <p>Procedencia: ${data.location.city}</p>
     <a href= "${data.picture}foto de perfil</a>
     </li>`;
- 
   });
 
-window.onload = obtainDates()
-obtainDates()
-contenido.innerHTML += html;
-
+  contenido.innerHTML += html;
 }
+
+document.addEventListener("DOMContentLoaded", obtainDates);
